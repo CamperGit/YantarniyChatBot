f890cd4e26605ff5bec22663ab158e0e9f67fa35
CamperGit <sashchenkodmitrii@mail.ru>
2021-07-02 13:57:31 +0300

    add webhook controller

--- src/main/java/com/camper/yantarniytelegrambot/appconfig/BotConfig.java
+++ src/main/java/com/camper/yantarniytelegrambot/appconfig/BotConfig.java
@@ -3,19 +3,24 @@ package com.camper.yantarniytelegrambot.appconfig;
 import com.camper.yantarniytelegrambot.YantarniyTelegramBot;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.telegram.telegrambots.bots.DefaultBotOptions;
 import org.telegram.telegrambots.meta.ApiConstants;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 
 @Configuration
-@ConfigurationProperties("telegramBot")
+@PropertySource("classpath:bot.properties")
+@ComponentScan
 public class BotConfig {
-
     @Bean
-    public YantarniyTelegramBot telegramBot(String webHookPath, String username, String token) {
+    public YantarniyTelegramBot telegramBot(@Value("${telegrambot.webHookPath}")String webHookPath,
+                                            @Value("${telegrambot.username}") String username,
+                                            @Value("${telegrambot.token}") String token) {
         return new YantarniyTelegramBot(new DefaultBotOptions(),webHookPath,username,token);
     }
 }
---
+++ src/main/java/com/camper/yantarniytelegrambot/controller/WebHookController.java
@@ -0,0 +1,24 @@
+package com.camper.yantarniytelegrambot.controller;
+
+import com.camper.yantarniytelegrambot.YantarniyTelegramBot;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+@RestController
+public class WebHookController {
+    private YantarniyTelegramBot telegramBot;
+
+    @PostMapping("/")
+    public BotApiMethod<?> onUpdateReceiver(@RequestBody Update update) {
+        return telegramBot.onWebhookUpdateReceived(update);
+    }
+
+    @Autowired
+    public void setTelegramBot(YantarniyTelegramBot telegramBot) {
+        this.telegramBot = telegramBot;
+    }
+}
