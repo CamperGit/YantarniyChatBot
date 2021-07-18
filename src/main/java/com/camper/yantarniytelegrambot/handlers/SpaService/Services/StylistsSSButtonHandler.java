d255318044a8b870c0366b60717a98bc91b9e0e3
CamperGit <77017039+CamperGit@users.noreply.github.com>
2021-07-18 18:35:13 +0300

    Temporaly fix empty database bug

--- src/main/java/com/camper/yantarniytelegrambot/handlers/SpaService/Services/StylistsSSButtonHandler.java
+++ src/main/java/com/camper/yantarniytelegrambot/handlers/SpaService/Services/StylistsSSButtonHandler.java
@@ -12,7 +12,7 @@ import java.util.List;
 public class StylistsSSButtonHandler extends AbstractSpaServiceButtonHandler {
     @Override
     protected String getLocationName() {
-        return "STYLISTS";
+        return "COSMETOLOGY";
     }
 
     @Override
