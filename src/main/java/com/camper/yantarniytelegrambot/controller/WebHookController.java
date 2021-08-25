d1542e4f7e37a1c05d5e25cdf88391c00388def0
CamperGit <sashchenkodmitrii@mail.ru>
2021-08-25 16:08:16 +0300

    create get mapping for kaffeine

--- src/main/java/com/camper/yantarniytelegrambot/controller/WebHookController.java
+++ src/main/java/com/camper/yantarniytelegrambot/controller/WebHookController.java
@@ -1,7 +1,9 @@
 package com.camper.yantarniytelegrambot.controller;
 
 import com.camper.yantarniytelegrambot.botapi.YantarniyTelegramBot;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,12 +11,18 @@ import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
 @RestController
+@Slf4j
 public class WebHookController {
     private YantarniyTelegramBot telegramBot;
 
     @PostMapping("/")
     public BotApiMethod<?> onUpdateReceiver(@RequestBody Update update) {
         return telegramBot.onWebhookUpdateReceived(update);
+    }
+
+    @GetMapping
+    public void kaffeineUpdate() {
+        log.debug("Kaffein ping task");
     }
 
     @Autowired
